fecf4eda57981540b71a337004e92dd100e8c660
GigaMatt <msmontoya1996@gmail.com>
2020-12-07 11:25:08 -0500

    Added initial TF2.0 test script

---
+++ tensorflow_arm_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""tensorflow_arm_test.py docstring.
+
+Purpose:
+    This module demonstrates the applicability of using tensorflow on:
+        1. macOS 11.0.1 (arm) emulated over Rosetta2.
+        2. APL-provision macOS configurations.
+    Successful demonstration of [1][2] proves the viability of using this configuration for WHITTER.
+
+Example:
+    $ python3 tensorflow_arm_test.py
+
+Attributes:
+    Module_level_variable1 (int): Module level variables may be documented in an inline docstring
+        immediately following the variable.
+
+References:
+    This styleguide follows the Google Python Style Guide:
+        http://google.github.io/styleguide/pyguide.html
+"""
+
+#import tensorflow as tf
+#print(tf.__version__)
+
+print("Hello world")
